6424b795d3eb915fbef4a2ac46730f01bedf302e
mukul1107 <mukulsingh3078@gmail.com>
2024-04-18 14:06:30 +0530

    Values can be fetched now✅

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ let btn = document.querySelector("button")
 const input = document.querySelector(".input-field")
 let username;
 
+let userObj = {
+    name: username,
+    followers: 0,
+    following: 0,
+    avatar: "",
+    repository: 0,
+    star: 0
+}
+
+
+
+const userCard = document.createElement("div")
+userCard.setAttribute("class","user-card");
+
+
+
+
+
 btn.addEventListener("click", (event) => {
     event.preventDefault();
-    username = String(input.value)
+    username = String(input.value).toLowerCase()
     // console.log("Username:", username);
     getDataFromGithub(username)
 })
@@ -24,6 +42,10 @@ function getDataFromGithub(name) {
             return response.json();
         })
         .then(data => {
+            userObj["name"] = data.name;
+            userObj["followers"] = data.followers;
+            userObj["following"] = data.following;
+            userObj["repository"] = data.public_repos;
             console.log(
                 `Username is: ${data.login}\nNumber of followers: ${data.followers}\nNumber of following: ${data.following}`
             )
---
+++ test.js
@@ -0,0 +1,9 @@
+let obj = {
+    Uname: "kajdksj",
+    pass: "akjfk"
+}
+console.log(obj)
+obj["Uname"] = "damon"
+obj["pass"] = "secret"
+
+console.log(obj)
